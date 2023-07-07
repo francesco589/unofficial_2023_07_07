231c7d45fb131ef0ded4c908ed67934372d4aa65
Francesco Accardo <fra89@ymail.com>
2023-07-07 17:11:01 +0200

    third

--- testapp/src/Appex1.jsx
+++ testapp/src/Appex1.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ShowStuds from './componentsex1/ShowStuds'
 
 function Appex1() {
+
   const [studs, setStuds] = useState ([
     {id: 1, fullname: "mario rossi", avg: 6, gender: 'm'}, 
     {id: 2, fullname: "luigi verdi", avg: 4, gender: 'm'}, 
     {id: 3, fullname: "peach pink", avg: 8, gender: 'f'},
     {id: 4, fullname: "wario rossi", avg: 9, gender: 'm'},
-    {id: 5, fullname: "daisy princess", avg: 10, gender: 'f'}, 
+    {id: 5, fullname: "daisy princess", avg: 10, gender: 'f'} 
     ])
 
-    function printStud(arr){
-      arr.map(el => {
-        return(<div key={el.id}><span>Fullname: {el.fullname}</span><span>Average: {el.avg}</span>{el}</div>)
-      })
-    }
-
+  console.log(studs)
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center'>
-      {printStud(studs)}
+      <section className=' h-screen flex flex-col items-center justify-center gap-6 '>
+        <input className='border border-black rounded-l' type="text" />
+      <div className=' bg-slate-600 flex flex-col w-52 p-3 rounded-xl  items-center justify-center gap-5'>
+        
+    <ShowStuds studs={studs} />
+        
       </div>
+      </section>
     </>
   )
 }
